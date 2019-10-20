0c5f2aba4e1e5110d76aaa178d6fe21c12449293
Jocelyn Song <zqsong17@fudan.edu.cn>
2019-10-20 18:50:29 +0800

    Delete test_bleu.sh

--- experiment/test_bleu.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-/home/develop/disks/sdb/Songzq/mosesdecoder/scripts/generic/multi-bleu.perl \
--lc /home/develop/disks/sdb/Songzq/emotional_chat_machine/data/stc_data/train_test/test.response.filter.txt \
-< /home/develop/disks/sdb/Songzq/emotional_chat_machine/exp/test/response.e200.lr0.1.1e-1.txt 
